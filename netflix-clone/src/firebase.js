77cf915dc22a354b415da11f4d9019d0131f89ae
remi abduls <remiabduls@gmail.com>
2021-12-01 19:38:03 +0800

    commit to 'main'

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAvl4JYjEhfM8TJJSykWqZ6iELWGfptoiM",
+  authDomain: "netflix-clone-f0a4c.firebaseapp.com",
+  projectId: "netflix-clone-f0a4c",
+  storageBucket: "netflix-clone-f0a4c.appspot.com",
+  messagingSenderId: "312279268231",
+  appId: "1:312279268231:web:a3a3c5d4ecbdcce4b27122",
+};
